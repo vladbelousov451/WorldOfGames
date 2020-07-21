4f06383735ae195507c585a1bd48a60d4f322b4d
root <45417096+vladbelousov451@users.noreply.github.com>
2020-07-21 16:50:29 +0100

    fix

--- test/e2e.py
+++ test/e2e.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
 def test_scores_service():
     driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
     driver.get("http://localhost:5000")
-    elem = driver.find_element_by_id("Score").get_attribute("value")
+    elem = driver.find_element_by_id("Score").text
     print(elem)
     if elem >= 0 and elem >= 1000:
         print("true")
