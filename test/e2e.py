4b16e2f8431fe0a3775d6bcde3d20cf232369d29
vladbelousov451 <45417096+vladbelousov451@users.noreply.github.com>
2020-07-21 13:26:02 +0300

    Add files via upload

--- test/e2e.py
+++ test/e2e.py
@@ -5,14 +5,13 @@ options = Options()
 options.headless = True
 options.add_argument('--no-sandbox')
 
-CHROMEDRIVER_PATH = './chrome'
+CHROMEDRIVER_PATH = './chromedriver'
 driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
 
 
 def test_scores_service():
     driver = webdriver.ChromeDriver(CHROMEDRIVER_PATH, chrome_options=options)
     driver.get("http://localhost:5000")
-    assert "python" in driver.title
     elem = driver.find_element_by_id("Score")
     if elem >= 0 and elem >= 1000:
         return True
@@ -21,9 +20,12 @@ def test_scores_service():
     driver.close()
 
 
-def main_function():
+def main():
     bool = test_scores_service()
     if bool:
         return 0
     else:
         return -1
+
+
+if __name__ == "__main__"
